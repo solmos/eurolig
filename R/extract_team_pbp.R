d8c98a8b0fdc4e244cf1f3b34a4991be86dac67f
Sergio Olmos <s.olmos.pardo@gmail.com>
2019-03-09 00:34:17 +0100

    Refixed bug

--- R/extract_team_pbp.R
+++ R/extract_team_pbp.R
@@ -12,6 +12,6 @@
 extract_team_pbp <- function(team, season) {
     game_results <- scrape_team_results(team, season)
     game_codes <- game_results$game_code
-    pbp <- extract_pbp(game_codes, season_code)
+    pbp <- extract_pbp(game_codes, season)
     pbp
 }
