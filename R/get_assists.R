bc75cea664e251f415cb4aed9e1a03851e9b0981
Sergio Olmos <s.olmos.pardo@gmail.com>
2019-03-17 21:28:21 +0100

    Added option to find and1s

--- R/get_assists.R
+++ R/get_assists.R
@@ -6,14 +6,14 @@
 #' @export
 #'
 #' @examples
-get_assists <- function(df, team) {
-    df <- tibble::rowid_to_column(df, "row_number")
+get_assists <- function(df, team, and1 = FALSE) {
     if (!missing(team)) {
         df <- df %>%
             dplyr::filter(team_code == team,
                           play_type != "IN",
                           play_type != "OUT")
     }
+    df <- tibble::rowid_to_column(df, "row_number")
     # Assisted FGM are recorded in the row above the assist row
     assists_idx <- which(df$play_type == "AS")
     fg_idx <- assists_idx - 1
@@ -25,9 +25,11 @@ get_assists <- function(df, team) {
         team_code = df$team_code[fg_idx],
         seconds = df$seconds[fg_idx],
         game_code = df$game_code[fg_idx],
-        season_code = df$season[fg_idx],
+        season = df$season[fg_idx],
         play_number = df$play_number[assists_idx],
-        df_idx = assists_idx,
+        time_remaining = df$time_remaining[fg_idx],
+        quarter = df$quarter[fg_idx],
+        row_number = df$row_number[assists_idx],
         stringsAsFactors = FALSE
     )
 
@@ -46,38 +48,45 @@ get_assists <- function(df, team) {
 
     # Dealing with "and 1s"
     ## TODO: SIMPLIFY/VECTORIZE THIS CODE AT SOME POINT
-    and1_df <- assists_df %>%
+    and1_key <- assists_df %>%
         dplyr::filter(play_type == "RV" | play_type == "CV") %>%
-        dplyr::select(df_idx, play_number, game_code, play_type)
-    and1_playtype <- character(nrow(and1_df))
-    and1_ft <- character(nrow(and1_df))
-    for (i in 1:nrow(and1_df)) {
-        play_no <- and1_df[i, "play_number"]
-        game_no <- and1_df[i, "game_code"]
-        # FG is three plays before the assist (after RV and CV)
-        fg_idx <- which(df$play_number == (play_no - 3) &
-                         df$game_code == game_no)
-        and1_playtype[i] <- as.character(df$play_type[fg_idx])
-        # FT is one play after the assist
-        ft_idx <- fg_idx + 3
-        and1_ft[i] <- as.character(df$play_type[ft_idx])
+        dplyr::select(play_type, time_remaining, quarter, game_code)
+
+    and1_df <- df %>%
+        dplyr::filter(time_remaining %in% and1_key$time_remaining &
+                          quarter %in% and1_key$quarter &
+                          game_code %in% and1_key$game_code) %>%
+        dplyr::group_by(game_code, quarter, time_remaining) %>%
+        dplyr::summarise(fg2 = sum(play_type == "2FGM"),
+                         fg3 = sum(play_type == "3FGM"),
+                         ft = sum(play_type == "FTM"))
+
+    # The FGM is either a two or a three
+    # so from just the 2fg column we can find out the shot type
+    fg2 <- and1_df$fg2
+    and1_play_type <- dplyr::case_when(fg2 == 1 ~ "2FGM",
+                                       fg2 == 0 ~ "3FGM")
+
+    # Should we specify wheter the shot was an and1 in this type of analysis?
+    # Perhaps as an option of the function?
+    if (and1 == TRUE) {
+        and1_play_type <- paste0(and1_play_type, "+1")
+        and1_ft <- and1_df$ft
+        and1_points <- integer(nrow(and1_df))
+        and1_points[and1_play_type == "2FGM+1"] <- 2
+        and1_points[and1_play_type == "3FGM+1"] <- 3
+        and1_points <- and1_points + and1_ft
     }
-    and1_playtype <- paste0(and1_playtype, "+1")
+
     and1_idx <- which(assists_df$play_type == "RV" |
                           assists_df$play_type == "CV")
-    assists_df[and1_idx, "play_type"] <- and1_playtype
-    assists_df$play_type <- factor(assists_df$play_type)
+    assists_df[and1_idx, "play_type"] <- and1_play_type
 
     # Assign corresponding points to each play type
     assists_df$points[assists_df$play_type == "2FGM"] <- 2
     assists_df$points[assists_df$play_type == "3FGM"] <- 3
     assists_df$points[assists_df$play_type == "FTM"] <- assisted_ft_points
+    assists_df$points[and1_idx] <- and1_points
 
     tibble::as_tibble(assists_df)
 }
-
-x <- get_assists(pbp, "MAD")
-levels(x$play_type)
-df[5114:5119,]
-which(df$play_number == 487 & df$game_code == 166)
-df[4225:4229,]
