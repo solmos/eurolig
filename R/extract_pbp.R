7aa5ec51f535afffc84c70476166cb0d46aac98f
Sergio Olmos <s.olmos.pardo@gmail.com>
2019-03-25 11:06:46 +0100

    Added code to deal with over times

--- R/extract_pbp.R
+++ R/extract_pbp.R
@@ -1,13 +1,20 @@
-#' Extract play-by-play data for Euroleague games
+#' Extract play-by-play data
+#'
+#' \code{extract_pbp} downloads play-by-play data from Euroleague and returns
+#' a tidy data frame.
+#'
+#'
 #'
 #' @param game_code An integer as specified in the game url
-#' @param season The starting year of the desired season
+#' @param season An integer specifying the starting year of the desired season
 #'
 #' @importFrom magrittr %>%
 #'
 #' @importFrom rlang .data
 #'
-#' @return A data frame
+#' @return A tibble
+#'
+#'
 #' @export
 #'
 #' @examples
@@ -64,22 +71,45 @@ extract_pbp <- function(game_code, season) {
     # Specify the score at every play using helper function add_points
     pbp_list <- lapply(pbp_list, add_points)
 
+    # Dealing with double (or more) overtimes
+    recode_overtimes <- function(x) {
+        x$QUARTER <- as.integer(x$QUARTER)
+        over_times <- sum(x$PLAYTYPE == "BP") - 4
+        if (over_times > 1) {
+            t <- 45
+            period <- 6
+            for (i in 1:(over_times - 1)) {
+                x$QUARTER[x$MINUTE > t] <- period
+                t <- t + 5
+                period <- period + 1
+            }
+        }
+        x
+    }
+    pbp_list <- lapply(pbp_list, recode_overtimes)
 
     pbp_df <- do.call("rbind", pbp_list)
 
     # The time when a period/game begins or ends is missing
-    pbp_df[which(pbp_df$PLAYTYPE == "BP"), "MARKERTIME"] <- "10:00"
+    begin_quarter_idx <- which(pbp_df$PLAYTYPE == "BP" &
+                                   as.integer(pbp_df$QUARTER) <= 4)
+    begin_ot_idx <- which(pbp_df$PLAYTYPE == "BP" &
+                              as.integer(pbp_df$QUARTER) > 4)
+    pbp_df[begin_quarter_idx, "MARKERTIME"] <- "10:00"
+    pbp_df[begin_ot_idx, "MARKERTIME"] <- "05:00"
     pbp_df[which(pbp_df$PLAYTYPE == "EP"), "MARKERTIME"] <- "00:00"
     pbp_df[which(pbp_df$PLAYTYPE == "EG"), "MARKERTIME"] <- "00:00"
 
     # Create a variable specifying the seconds elapsed since start of game:
     #   Each quarter has 600 seconds
     #   Each over time has 300 seconds
-
-    seconds_per_quarter <- 600 * as.numeric(pbp_df$QUARTER)
-    # TODO: Deal with over times
+    quarters_idx <- which(pbp_df$QUARTER <= 4)
+    ot_idx <- which(pbp_df$QUARTER > 4)
+    seconds_per_quarter <- 600 * pbp_df$QUARTER[quarters_idx]
+    seconds_per_ot <- 2400 + 300 * (pbp_df$QUARTER[ot_idx] - 4)
+    s <- c(seconds_per_quarter, seconds_per_ot)
     seconds_remaining_in_q <- as.numeric(lubridate::ms(pbp_df$MARKERTIME))
-    seconds_elapsed <- seconds_per_quarter - seconds_remaining_in_q
+    seconds_elapsed <- s - seconds_remaining_in_q
 
 
     pbp <- pbp_df %>%
@@ -90,10 +120,10 @@ extract_pbp <- function(game_code, season) {
             player_name = factor(.data$PLAYER, exclude = ""),
             play_type = factor(.data$PLAYTYPE),
             time_remaining = .data$MARKERTIME,
-            quarter = .data$QUARTER,
+            quarter = factor(.data$QUARTER),
             points_home = .data$POINTS_HOME,
             points_away = .data$POINTS_AWAY,
-            team_name = factor(.data$TEAM, exclude = ""),
+            team_name = factor(.data$TEAM, exclude = NA),
             player_id = factor(trimws(.data$PLAYER_ID), exclude = ""),
             player_dorsal = as.numeric(.data$DORSAL),
             play_info = .data$PLAYINFO,
